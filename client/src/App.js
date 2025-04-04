1ee740799a444a2c6df36183fca190e2d8a2f242
baliddeki <baliddeki@gmail.com>
2025-04-05 00:58:17 +0300

    completed end points and front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,3 @@
-// client/src/App.js
-
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -8,17 +6,15 @@ function App() {
   const [actions, setActions] = useState([]);
 
   useEffect(() => {
-    // Fetch events from external API
     axios
-      .get("https://cms.klanlogistics.com:8443/api/wylon-apis/protected?passcode=wylon2025access")
-      .then((res) => setEvents(res.data))
+      .get(
+        "https://cms.klanlogistics.com:8443/api/wylon-apis/protected?passcode=wylon2025access"
+      )
+      .then((res) => {
+        console.log("API data:", res.data);
+        setEvents(res.data.data); // Correct: res.data.data is the array
+      })
       .catch((err) => console.error("Error fetching violations", err));
-
-    // Fetch saved actions from local DB
-    axios
-      .get("http://localhost:5000/api/actions")
-      .then((res) => setActions(res.data))
-      .catch((err) => console.error("Error fetching actions", err));
   }, []);
 
   const getActionForEvent = (eventId) => {
@@ -32,26 +28,38 @@ function App() {
       <table border="1" cellPadding="10" style={{ marginTop: "2rem", width: "100%" }}>
         <thead>
           <tr>
-            {events.length > 0 &&
-              Object.keys(events[0]).map((key) => <th key={key}>{key}</th>)}
+            <th>#</th>
+            <th>Vehicle</th>
+            <th>Violation</th>
+            <th>Time</th>
+            <th>Location</th>
+            <th>Value</th>
+            <th>Avg Speed</th>
             <th>Actions Taken</th>
-            <th>Action</th>
+            <th>Add Action</th>
           </tr>
         </thead>
         <tbody>
-          {events.map((event, index) => (
-            <tr key={index}>
-              {Object.values(event).map((value, idx) => (
-                <td key={idx}>{value}</td>
-              ))}
-              <td>{getActionForEvent(event.id)}</td>
-              <td>
-                <button onClick={() => alert(`Open modal for ${event.id}`)}>
-                  Add Action
-                </button>
-              </td>
-            </tr>
-          ))}
+          {events.map((event, index) => {
+            const violation = event.attributes;
+            return (
+              <tr key={event.id}>
+                <td>{index + 1}</td>
+                <td>{violation.vcode}</td>
+                <td>{violation.violation}</td>
+                <td>{violation.beginningtime}</td>
+                <td>{violation.initiallocation}</td>
+                <td>{violation.value}</td>
+                <td>{violation.averagespeed}</td>
+                <td>{getActionForEvent(event.id)}</td>
+                <td>
+                  <button onClick={() => alert(`Open modal for event ${event.id}`)}>
+                    Add Action
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
